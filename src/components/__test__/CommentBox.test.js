973a46037bae17aa0588c675f372a326265bb894
Ove <osundal83@gmail.com>
2018-08-09 23:41:56 +0200

    add CommentBox tests

--- src/components/__test__/CommentBox.test.js
+++ src/components/__test__/CommentBox.test.js
@@ -15,5 +15,28 @@ afterEach(() => {
 it('has a text area and a button', () => {
     expect(wrapped.find('textarea').length).toEqual(1);
     expect(wrapped.find('button').length).toEqual(1);
+});
+
+//use describe to scope before/afterEach to some tests, but not all
+describe('the text area', () => {
+    //beforeEach is evaluated only inside the describe function
+    beforeEach(() => {
+        wrapped.find('textarea').simulate('change', {
+            target: {value: 'comment1'}
+        });
+        //force synchronous rerender
+        wrapped.update();
+    });
+
+    it('has a text area that users can type in', () => {
+        expect(wrapped.find('textarea').prop('value')).toEqual('comment1')
+    });
+
+    it('when form is submitted, text area is emptied', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('comment1');
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+});
 
 });
