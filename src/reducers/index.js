bf7a927f679eec48b9f7e508ad9798cdf8ba2da6
Ove <osundal83@gmail.com>
2018-08-17 23:37:16 +0200

    add login button

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,8 @@
 import {combineReducers} from 'redux';
 import commentsReducer from 'reducers/comments';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
-import reducers from 'reducers';
+import authReducer from 'reducers/auth';    
 
 export default combineReducers({
-   comments: commentsReducer
+   comments: commentsReducer,
+   auth: authReducer
 });
