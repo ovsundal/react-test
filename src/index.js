e335d96dc2f54684165ecd0fd25f1afa75617e45
Ove <osundal83@gmail.com>
2018-08-17 23:08:21 +0200

    add routing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import Root from 'Root';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { BrowserRouter, Route } from "react-router-dom";
+import Root from "Root";
 
 ReactDOM.render(
-    <Root>
-        <App/>
-    </Root>,
-    document.querySelector('#root')
+  <Root>
+    <BrowserRouter>
+      <Route path="/" component={App} />
+    </BrowserRouter>
+  </Root>,
+  document.querySelector("#root")
 );
