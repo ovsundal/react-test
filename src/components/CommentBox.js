1589f18bb0c3e1baebf1f0344afb9d278681840a
Ove <osundal83@gmail.com>
2018-08-19 00:19:35 +0200

    add HOC(requireAuth)

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as actions from "actions";
+import requireAuth from 'components/requireAuth';
 
 class CommentBox extends Component {
   state = { comment: "" };
+
+
 
   handleChange = event => {
     this.setState({ comment: event.target.value });
@@ -33,7 +36,8 @@ class CommentBox extends Component {
   }
 }
 
+
 export default connect(
   null,
   actions
-)(CommentBox);
+)(requireAuth(CommentBox));
---
+++ src/components/requireAuth.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import {connect} from 'react-redux';
+
+export default ChildComponent => {
+  class ComposedComponent extends Component {
+    componentDidMount() {
+      this.shouldNavigateAway();
+    }
+
+    componentDidUpdate() {
+      this.shouldNavigateAway();
+    }
+
+    shouldNavigateAway() {
+      if (!this.props.auth) {
+        console.log("I NEED TO LEAVE!!!");
+        this.props.history.push("/");
+      }
+    }
+
+    render() {
+      return <ChildComponent {...this.props} />;
+    }
+  }
+  
+function mapStateToProps(state) {
+    return {auth: state.auth};
+  }
+
+  return connect(mapStateToProps)(ComposedComponent);
+};
